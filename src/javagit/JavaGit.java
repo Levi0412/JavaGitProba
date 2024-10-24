7d1d43820eec5c8f00a73121000576deb44aec8b
Levi0412 <leventehorvath0426@gmail.com>
2024-10-24 15:57:42 +0200

    feldat megoldas: hagyomanyos if szerkezet

--- src/javagit/JavaGit.java
+++ src/javagit/JavaGit.java
@@ -4,7 +4,24 @@ package javagit;
 public class JavaGit {
 
     public static void main(String[] args) {
-       
+       /* 10 fok alatt van hideg 
+        ha hideg van, akkor
+        viszek kabátot
+        egyébként
+        nem viszek kabátot
+        */
+       final int HIDEG_HATAR = 10;
+       int fok = 7;
+       boolean hidegVan = fok < HIDEG_HATAR;
+       String eredmeny = "";
+       if(hidegVan)
+       {
+          eredmeny = "viszek kabátot";
+       }else{
+           eredmeny = "nem viszek kabátot";
+           
+           System.out.println(eredmeny);
+       }
     }
     
 }
